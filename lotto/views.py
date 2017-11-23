9b8f66f0e7a53a08f4c41d91717e41adbc56bb38
jace <jacealan1@gmail.com>
2017-11-24 02:29:58 +0900

    modified

--- lotto/views.py
+++ lotto/views.py
@@ -3,7 +3,7 @@ from lotto.models import Lotto
 
 # Create your views here.
 def lotto_list(request):
-    latest_list = Lotto.objects.all().order_by('-nth')[:30]
+    latest_list = Lotto.objects.all().order_by('-nth')[:10]
 
     context = {'latest_list': latest_list}
     return render(request, 'lotto/lotto_list.html', context)
