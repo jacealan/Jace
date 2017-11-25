318e4be648c38db8f72aefd9d8f90a29faaf6d1b
jace <jacealan1@gmail.com>
2017-11-26 01:35:59 +0900

    style font size 1.5em

--- eng/views.py
+++ eng/views.py
@@ -3,6 +3,9 @@ from eng.models import Sentences
 
 # Create your views here.
 def eng_today(request):
+    # sentence_list = Sentences.objects.filter(pk__in=[1,4,7])
+
+    # sentence_list[2]= Sentences.objects.filter(pk=2)
     sentence_list = Sentences.objects.all().order_by('?')[:10]
     context = {'sentence_list': sentence_list}
     return render(request, 'eng/eng_today.html', context)
