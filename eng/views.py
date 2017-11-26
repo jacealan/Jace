97910b362482ef7b34d51e5e01bc8499861c1fdd
jace <jacealan1@gmail.com>
2017-11-26 20:33:58 +0900

    color

--- eng/views.py
+++ eng/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from eng.models import Sentences
+from eng.models import Sentences, Words
 
 # Create your views here.
 def eng_today(request):
@@ -7,5 +7,6 @@ def eng_today(request):
 
     # sentence_list[2]= Sentences.objects.filter(pk=2)
     sentence_list = Sentences.objects.all().order_by('?')[:10]
-    context = {'sentence_list': sentence_list}
+    word_list = Words.objects.all().order_by('?')[:10]
+    context = {'sentence_list': sentence_list, 'word_list': word_list}
     return render(request, 'eng/eng_today.html', context)
