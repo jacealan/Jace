58e633047e290ba7accd4680918d064647e6d1c3
jace <jacealan1@gmail.com>
2017-11-26 21:53:27 +0900

    modified

--- eng/views.py
+++ eng/views.py
@@ -7,6 +7,6 @@ def eng_today(request):
 
     # sentence_list[2]= Sentences.objects.filter(pk=2)
     sentence_list = Sentences.objects.all().order_by('?')[:10]
-    word_list = Words.objects.all().order_by('?')[:10]
+    word_list = Words.objects.all().order_by('?')[:20]
     context = {'sentence_list': sentence_list, 'word_list': word_list}
     return render(request, 'eng/eng_today.html', context)
